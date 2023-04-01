da840d27e3e69e15d6b81f87c5ad5e41bf7e3530
m4chado <enzo653fm@gmail.com>
2023-03-31 22:33:02 -0300

    FrontEnd: next.js and tailwind initial setup.

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import { Providers } from "@/components/Providers";
+import "@/styles/globals.css";
+import { useTheme } from "next-themes";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import { Inter } from "next/font/google";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-roboto" });
+
+export default function App({ Component, pageProps }: AppProps) {
+	const { theme } = useTheme();
+
+	return (
+		<Providers>
+			<main
+				className={`h-screen ${inter.variable} font-sans text-gray-700 dark:text-gray-100 bg-[url('../../public/bg-light.jpg')] bg-cover dark:bg-[url('../../public/bg-dark.jpg')] transition duration-300 overflow-auto`}
+			>
+				<Component {...pageProps} />
+				<ToastContainer
+					theme={theme === "light" ? "light" : "dark"}
+					position="bottom-right"
+					autoClose={2000}
+				/>
+			</main>
+			{/* <ReactQueryDevtools initialIsOpen={true} /> */}
+		</Providers>
+	);
+}
