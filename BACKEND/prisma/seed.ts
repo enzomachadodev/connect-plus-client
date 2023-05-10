e15342164fb8915a539c3ac8d693bddb8270520f
Enzo <enzo653fm@gmail.com>
2023-05-10 20:58:08 -0300

    Added project preview image

--- BACKEND/prisma/seed.ts
+++ BACKEND/prisma/seed.ts
@@ -4,34 +4,52 @@ import { hash } from "bcrypt";
 
 const prisma = new PrismaClient();
 
+const generatedNumbers = new Set();
+
+const numberGenerate = () => {
+	let phoneNumber = "";
+
+	do {
+		phoneNumber = "9";
+
+		for (let i = 0; i < 8; i++) {
+			phoneNumber += Math.floor(Math.random() * 10);
+		}
+	} while (generatedNumbers.has(phoneNumber));
+
+	generatedNumbers.add(phoneNumber);
+
+	return phoneNumber;
+};
+
 async function main() {
 	const hashPassword = await hash("12345", 10);
 	const user = await prisma.user.create({
 		data: {
 			name: "Enzo Machado",
-			email: "enzomachado@email.com",
+			email: "teste@email.com",
 			password: hashPassword,
 			avatarUrl: "https://github.com/m4chado.png",
 		},
 	});
 
-	for (let i = 0; i < 10; i++) {
+	for (let i = 0; i < 15; i++) {
 		const customer = await prisma.customer.create({
 			data: {
-				avatarUrl: faker.image.business(),
+				avatarUrl: faker.image.avatar(),
 				name: faker.company.name(),
-				email: faker.internet.email(),
-				phone: faker.phone.number(),
+				email: `customer-${i}@email.com`,
+				phone: numberGenerate(),
 				userId: user.id,
 			},
 		});
 
-		for (let j = 0; j < 3; j++) {
+		for (let j = 0; j < 5; j++) {
 			await prisma.contact.create({
 				data: {
 					name: faker.name.fullName(),
-					email: faker.internet.email(),
-					phone: faker.phone.number(),
+					email: `contact-${i}-${j}@email.com`,
+					phone: numberGenerate(),
 					avatarUrl: faker.image.avatar(),
 					customerId: customer.id,
 				},
